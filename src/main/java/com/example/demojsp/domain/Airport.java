6b4abbd43aebf0fcc02b78129532c6f53cb8c06d
potka <potka@example.com>
2021-06-24 01:25:32 +0800

    created list for all runways

--- src/main/java/com/example/demojsp/domain/Airport.java
+++ src/main/java/com/example/demojsp/domain/Airport.java
@@ -1,10 +1,14 @@
 package com.example.demojsp.domain;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Airport {
@@ -15,6 +19,9 @@ public class Airport {
     
     @Column(name = "AIRPORT_NAME", nullable = false)
     String airportName;
+
+    @OneToMany(mappedBy = "airport")
+    List<AirportRunway> airportRunways;
 
 
 
@@ -30,4 +37,11 @@ public class Airport {
     public void setAirportName(String airportName) {
         this.airportName = airportName;
     }
+    public List<AirportRunway> getAirportRunways() {
+        return airportRunways;
+    }
+    public void setAirportRunways(List<AirportRunway> airportRunways) {
+        this.airportRunways = airportRunways;
+    }
+    
 }
